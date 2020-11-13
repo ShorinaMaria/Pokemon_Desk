11ff47ca965e19040514b4149c62c5ea771ca975
MARIYA SHORINA <mariyashorina@MacBook-Pro-MARIYA.local>
2020-11-13 13:11:10 +0300

    homework5 pokedex

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import pokemons from '../../pokemons';
 import Header from '../../components/Header';
 import PokemonCard from '../../components/PokemonCard';
+import Layout from '../../components/Layout';
+import s from './Pokedex.module.scss';
 
 const PokedexPage = () => (
   <div>
     <Header />
-    <ul>
-      {pokemons.map((el) => (
-        <li key={el.id}>
-          <PokemonCard pokemon={el} />
-        </li>
-      ))}
-    </ul>
+    <Layout>
+      <p className={s.pokemonsDescr}>
+        800 <b>Pokemons</b> for you to choose your favorite
+      </p>
+      <div className={s.pokemonsContainer}>
+        {pokemons.map((el) => (
+          <div key={el.id}>
+            <PokemonCard pokemon={el} />
+          </div>
+        ))}
+      </div>
+    </Layout>
   </div>
 );
 
