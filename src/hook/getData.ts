3e60f4ac3e7ed93f627104fb9ddca2d392aab754
MARIYA SHORINA <mariyashorina@MacBook-Pro-MARIYA.local>
2020-11-18 14:36:50 +0300

    homework7

---
+++ src/hook/getData.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import req from '../utils/request';
+
+interface IUseDataResponse<T> {
+  data: T;
+  isLoading: boolean;
+  isError: boolean;
+}
+
+const useData = <D>(endpoint: string, query: object, deps: Array<any> = []): IUseDataResponse<D | null> => {
+  const [data, setData] = useState<D | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      try {
+        const result = await req(endpoint, query);
+        setData(result);
+      } catch (e) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getData();
+  }, deps);
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+};
+
+export default useData;
--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,78 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PokemonCard, { IPokemon } from '../../components/PokemonCard';
 import Layout from '../../components/Layout';
 import s from './Pokedex.module.scss';
+import useData from '../../hook/getData';
 
 interface IPokemonsData {
   total: number;
   pokemons: Array<IPokemon>;
 }
 
-const usePokemons = () => {
-  const [data, setData] = useState<IPokemonsData>({ total: 0, pokemons: [] });
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const getPokemons = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(`http://zar.hosthot.ru/api/v1/pokemons?limit=100`);
-        const result = await response.json();
-        setData(result);
-      } catch (e) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    getPokemons();
-  }, []);
-
-  return {
-    data,
-    isLoading,
-    isError,
-  };
-};
-
 const PokedexPage = () => {
-  const { data, isLoading, isError } = usePokemons();
+  const [searchValue, setSearchValue] = useState(``);
+  const [query, setQuery] = useState({});
+  const { data, isLoading, isError } = useData<IPokemonsData>(`getPokemons`, query, [searchValue]);
 
-  if (isLoading) {
-    return (
-      <div>
-        <Layout>
-          <p>Loading...</p>
-        </Layout>
-      </div>
-    );
-  }
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    setQuery((value) => ({
+      ...value,
+      name: e.target.value,
+    }));
+  };
 
-  if (isError) {
-    return (
-      <div>
-        <Layout>
-          <p>Error!</p>
-        </Layout>
-      </div>
-    );
-  }
+  const isLoaded = () => !isLoading && !isError;
 
   return (
     <div>
       <Layout>
         <p className={s.pokemonsDescr}>
-          {data.total} <b>Pokemons</b> for you to choose your favorite
+          {data && data.total ? data.total : `No`} <b>Pokemons</b> for you to choose your favorite
         </p>
-        <div className={s.pokemonsContainer}>
-          {data.pokemons.map((el) => (
-            <div key={el.id}>
-              <PokemonCard pokemon={el} />
-            </div>
-          ))}
-        </div>
+        <p>
+          <input type="text" value={searchValue} onChange={handleSearchChange} />
+        </p>
+
+        <p>
+          {isLoading ? 'Loading...' : null}
+          {isError ? 'Error!' : null}
+        </p>
+
+        {isLoaded() ? (
+          <div className={s.pokemonsContainer}>
+            {data.pokemons.map((el) => (
+              <div key={el.id}>
+                <PokemonCard pokemon={el} />
+              </div>
+            ))}
+          </div>
+        ) : null}
       </Layout>
     </div>
   );
